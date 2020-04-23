2202b7300fb34dd134b996813e8d685cc91f93c3
Tim <paananenenanaap@gmail.com>
2020-04-23 14:59:22 +1000

    initial commit

---
+++ CsvReader/CsvReader/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CsvReader
+{
+    internal class Person
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Gender { get; set; }
+        public string IpAddress { get; set; }
+    }
+}
---
+++ CsvReader/CsvReader/Program.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace CsvReader
+{
+    internal static class Program
+    {
+        private static void Main()
+        {
+            var lines = File.ReadAllLines(@"content\MOCK_DATA.csv");
+            var people = new List<Person>();
+            foreach (var line in lines)
+            {
+                var x = line.Split(',');
+                var person = new Person()
+                {
+                    Id = Convert.ToInt32(x[0]),
+                    FirstName = x[1],
+                    LastName = x[2],
+                    Email = x[3],
+                    Gender = x[4],
+                    IpAddress = x[5]
+                };
+                people.Add(person);
+                Console.WriteLine(JsonConvert.SerializeObject(person, new JsonSerializerSettings{ NullValueHandling = NullValueHandling.Include, Formatting = Formatting.Indented }));
+            }
+
+            Console.WriteLine($"{people.Count} entries processed.");
+        }
+    }
+}
